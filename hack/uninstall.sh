675504071801a8f7f634d947ca0797e82062f328
Ricardo Lourenco <rlourenc@redhat.com>
2021-02-04 16:05:50 +0100

    Update uninstall.sh

--- hack/uninstall.sh
+++ hack/uninstall.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+noobaa_ns=noobaa-operator
+for project in minio-operator velero; do oc delete project $project &> /dev/null; done
+
+oc patch bucketclass -n $noobaa_ns --type=merge -p '{"metadata":{"finalizers":null}}' noobaa-default-bucket-class
+oc patch noobaa -n $noobaa_ns --type=merge -p '{"metadata":{"finalizers":null}}' noobaa
+
+for pod in `oc get pods -o jsonpath='{.items[*].metadata.name}' -n $noobaa_ns`;do oc patch pod -n $noobaa_ns --type=merge -p '{"metadata":{"finalizers":null}}' $pod ;done
+../roles/noobaa_operator/files/noobaa uninstall --cleanup=true -n $noobaa_ns >> /tmp/noobaa_uninstall.log 2>&1
